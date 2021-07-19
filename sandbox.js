f87f8e093ce88274d4e685b5d84dea30de1f2025
Prajjwal Datir <prajjwaldatir0072000@gmail.com>
2021-07-19 17:16:56 +0530

    lesson 3

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,18 @@
 var character = "monarch";
 console.log(character);
 var inputs = document.querySelectorAll("input");
-console.log(inputs);
+inputs.forEach(function (input) { return console.log(input); });
+// Lesson 3
+var a = "a";
+var age = 13;
+//  chagne a 
+a = "b";
+// change age
+age = 14;
+// congraulations
+console.log(a);
+console.log(age);
+var circumferencesCircle = function (diameter) {
+    return Math.PI * diameter;
+};
+console.log(circumferencesCircle(10));
--- sandbox.ts
+++ sandbox.ts
@@ -3,3 +3,19 @@ console.log(character);
 
 const inputs = document.querySelectorAll("input");
 inputs.forEach((input) => console.log(input));
+
+// Lesson 3
+
+let a = "a";
+let age = 13;
+//  chagne a 
+a = "b";
+// change age
+age = 14;
+// congraulations
+console.log(a);
+console.log(age);
+const circumferencesCircle = (diameter : number) => {
+    return Math.PI * diameter;
+}
+console.log(circumferencesCircle(10));
